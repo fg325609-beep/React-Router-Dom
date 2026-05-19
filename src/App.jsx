0a3134ba5c0c9c13ce8e49206eabc04221df4260
Farhod Gofurov <fg325609@gmail.com>
2026-05-19 18:06:41 +0500

    llll

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AboutPages from './pages/AboutPages/AboutPages'
 import ContactPages from './pages/ContactPages/ContactPages'
 import ServicesPages from './pages/ServicesPages/ServicesPages'
-import Layout from './Companents/Layout/Layout'
-Layout
+import Layout from './Companents/Layout/Layout' // Papka nomini to'g'ri yozganingizga ishonch hosil qiling (Components bo'lishi kerak aslida)
+
 const App = () => {
   return (
     <div>
-     <BrowserRouter>
-            <Routes element={<Layout/>}>
-             <Route>
-               <Route path='about' element={<AboutPages/>}/>
-              <Route path='contact' element={<ContactPages/>}/>
-              <Route path='services'element={<ServicesPages/>}/>
-             </Route>
-            </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          {/* Layout ni asosiy Route qilib belgilaymiz */}
+          <Route path="/" element={<Layout />}>
+            {/* Ichki sahifalar */}
+            <Route path="about" element={<AboutPages />} />
+            <Route path="contact" element={<ContactPages />} />
+            <Route path="services" element={<ServicesPages />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/Companents/Header/Header.jsx
+++ src/Companents/Header/Header.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Header.scss' // SCSS faylini import qilamiz
+
 const Header = () => {
   return (
-    <div>
-        <h1>logo</h1>
-        <ul>
-            <li><Link to={about}></Link>Abaut</li>
-            <li><Link to={contact}></Link>Contact</li>
-            <li><Link to={services}></Link>Services</li>
-            </ul>
-    </div>
+    <header className="header">
+      <div className="header__container">
+        <div className="header__logo">
+          <Link to="/">logo</Link>
+        </div>
+        
+        <nav className="header__nav">
+          <ul className="header__list">
+            <li className="header__item">
+              <Link to="/about" className="header__link">About</Link>
+            </li>
+            <li className="header__item">
+              <Link to="/contact" className="header__link">Contact</Link>
+            </li>
+            <li className="header__item">
+              <Link to="/services" className="header__link">Services</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="header__actions">
+          <button type="button" className="header__btn">Submit</button>
+        </div>
+      </div>
+    </header>
   )
 }
 
